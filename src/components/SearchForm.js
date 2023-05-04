7c79d5dc706aba3a6613dfd615e27a705d71b9e4
dhruv5gohel <dhruv5gohel@gmail.com>
2023-05-04 19:47:11 +0530

    Edited Search Placeholder

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -16,7 +16,7 @@ const SearchForm = ({ handleSubmit }) => {
     return (
         <div>
             <form onSubmit={formSubmit}>
-                <input type="text" name="search" id="search" placeholder="Enter Movie or an Actor name" onChange={handleInput} value={inpVal} />
+                <input type="text" name="search" id="search" placeholder={searchType==="shows"?"Enter Show Name":"Enter Actor Name"} onChange={handleInput} value={inpVal} />
                 <div className="radio-container">
                     <label><input type="radio" name="searchOption" value="shows" className="inp-radio" onChange={(e) => { setSearchType(e.target.value) }} checked={searchType === "shows"} />Shows</label>
                     <label><input type="radio" name="searchOption" value="actors" className="inp-radio" onChange={(e) => { setSearchType(e.target.value) }} checked={searchType === "actors"} />Actors</label>
